2c7c5ac77030efb79b5e9588c012fb6a78ea8934
manucorsu <manuelcorsunsky@gmail.com>
2026-08-23 03:01:05 -0300

    fmt

--- pygal-stubs/_compat.pyi
+++ pygal-stubs/_compat.pyi
@@ -3,6 +3,7 @@ from typing import Any, Iterable
 
 from typing_extensions import TypeGuard
 
-
-def is_list_like(value: object) -> TypeGuard[Iterable[Any]]: ...  # pyright: ignore[reportExplicitAny]
+def is_list_like(
+    value: object,
+) -> TypeGuard[Iterable[Any]]: ...  # pyright: ignore[reportExplicitAny]
 def timestamp(x: datetime.datetime) -> float: ...
