492fff72715cc9237df4cef69280e6399a0b1319
manucorsu <manuelcorsunsky@gmail.com>
2026-08-23 18:06:14 -0300

    stubs for config

--- pygal-stubs/config.pyi
+++ pygal-stubs/config.pyi
@@ -1,131 +1,142 @@
-from _typeshed import Incomplete
+from types import FunctionType
+from typing import Self, TypeVar, Generic, Callable
 from pygal import formatters as formatters
 from pygal.interpolate import INTERPOLATIONS as INTERPOLATIONS
 from pygal.style import DefaultStyle as DefaultStyle, Style as Style
 
-CONFIG_ITEMS: Incomplete
-callable: Incomplete
+CONFIG_ITEMS: list[Key[object, object | None]]
+callable = FunctionType
+_T = TypeVar("_T", default=object)
+_SubtypeT = TypeVar("_SubtypeT", default=None)
 
-class Key:
-    value: Incomplete
-    type: Incomplete
-    doc: Incomplete
-    category: Incomplete
-    subdoc: Incomplete
-    subtype: Incomplete
+class Key(Generic[_T, _SubtypeT]):
+    value: _T | tuple[object, ...] | None
+    type: type[_T] | Callable[..., _T]
+    doc: str
+    category: str
+    subdoc: str
+    subtype: type[_SubtypeT] | Callable[[str], _SubtypeT] | None
     name: str
     def __init__(
-        self, default_value, type_, category, doc, subdoc: str = "", subtype=None
+        self,
+        default_value: _T | Callable[..., _T],
+        type_: type[_T] | Callable[..., _T],
+        category: str,
+        doc: str,
+        subdoc: str = "",
+        subtype: type[_SubtypeT] | Callable[[str], _SubtypeT] | None = None,
     ) -> None: ...
     @property
-    def is_boolean(self): ...
+    def is_boolean(self) -> bool: ...
     @property
-    def is_numeric(self): ...
+    def is_numeric(self) -> bool: ...
     @property
-    def is_string(self): ...
+    def is_string(self) -> bool: ...
     @property
-    def is_dict(self): ...
+    def is_dict(self) -> bool: ...
     @property
-    def is_list(self): ...
-    def coerce(self, value): ...
+    def is_list(self) -> bool: ...
+    def coerce(self, value: str) -> _T | str: ...
 
 class MetaConfig(type):
-    def __new__(mcs, classname, bases, classdict): ...
+    def __new__(
+        mcs, classname: str, bases: tuple[type, ...], classdict: dict[str, object]
+    ) -> MetaConfig: ...
 
-class BaseConfig(Incomplete):
-    def __init__(self, **kwargs) -> None: ...
-    def __call__(self, **kwargs) -> None: ...
-    def to_dict(self): ...
-    def copy(self): ...
+class BaseConfig(metaclass=MetaConfig):
+    def __init__(self, **kwargs: object) -> None: ...
+    def __call__(self, **kwargs: object) -> None: ...
+    def to_dict(self) -> dict[str, object]: ...
+    def copy(self) -> Self: ...
 
 class CommonConfig(BaseConfig):
-    stroke: Incomplete
-    show_dots: Incomplete
-    show_only_major_dots: Incomplete
-    dots_size: Incomplete
-    fill: Incomplete
-    stroke_style: Incomplete
-    rounded_bars: Incomplete
-    inner_radius: Incomplete
-    allow_interruptions: Incomplete
-    formatter: Incomplete
+    stroke: bool
+    show_dots: bool
+    show_only_major_dots: bool
+    dots_size: float
+    fill: bool
+    stroke_style: dict[str, object] | None
+    rounded_bars: int | None
+    inner_radius: float
+    allow_interruptions: bool
+    formatter: Callable[..., str] | None
 
 class Config(CommonConfig):
-    style: Incomplete
-    css: Incomplete
-    classes: Incomplete
-    defs: Incomplete
-    title: Incomplete
-    x_title: Incomplete
-    y_title: Incomplete
-    width: Incomplete
-    height: Incomplete
-    show_x_guides: Incomplete
-    show_y_guides: Incomplete
-    show_legend: Incomplete
-    legend_at_bottom: Incomplete
-    legend_at_bottom_columns: Incomplete
-    legend_box_size: Incomplete
-    rounded_bars: Incomplete
-    stack_from_top: Incomplete
-    spacing: Incomplete
-    margin: Incomplete
-    margin_top: Incomplete
-    margin_right: Incomplete
-    margin_bottom: Incomplete
-    margin_left: Incomplete
-    tooltip_border_radius: Incomplete
-    tooltip_fancy_mode: Incomplete
-    inner_radius: Incomplete
-    half_pie: Incomplete
-    reverse_direction: Incomplete
-    x_labels: Incomplete
-    x_labels_major: Incomplete
-    x_labels_major_every: Incomplete
-    x_labels_major_count: Incomplete
-    show_x_labels: Incomplete
-    show_minor_x_labels: Incomplete
-    y_labels: Incomplete
-    y_labels_major: Incomplete
-    y_labels_major_every: Incomplete
-    y_labels_major_count: Incomplete
-    show_minor_y_labels: Incomplete
-    show_y_labels: Incomplete
-    x_label_rotation: Incomplete
-    y_label_rotation: Incomplete
-    missing_value_fill_truncation: Incomplete
-    x_value_formatter: Incomplete
-    value_formatter: Incomplete
-    logarithmic: Incomplete
-    interpolate: Incomplete
-    interpolation_precision: Incomplete
-    interpolation_parameters: Incomplete
-    box_mode: Incomplete
-    order_min: Incomplete
-    min_scale: Incomplete
-    max_scale: Incomplete
-    range: Incomplete
-    secondary_range: Incomplete
-    xrange: Incomplete
-    include_x_axis: Incomplete
-    zero: Incomplete
-    no_data_text: Incomplete
-    print_values: Incomplete
-    dynamic_print_values: Incomplete
-    print_values_position: Incomplete
-    print_zeroes: Incomplete
-    print_labels: Incomplete
-    truncate_legend: Incomplete
-    truncate_label: Incomplete
-    js: Incomplete
-    disable_xml_declaration: Incomplete
-    force_uri_protocol: Incomplete
-    explicit_size: Incomplete
-    pretty_print: Incomplete
-    strict: Incomplete
-    no_prefix: Incomplete
-    inverse_y_axis: Incomplete
+    style: type[Style] | Style
+    css: list[str]
+    classes: list[str]
+    defs: list[str]
+    title: str | None
+    x_title: str | None
+    y_title: str | None
+    width: int
+    height: int
+    show_x_guides: bool
+    show_y_guides: bool
+    show_legend: bool
+    legend_at_bottom: bool
+    legend_at_bottom_columns: int | None
+    legend_box_size: int
+    rounded_bars: int | None
+    stack_from_top: bool
+    spacing: int
+    margin: int
+    margin_top: int | None
+    margin_right: int | None
+    margin_bottom: int | None
+    margin_left: int | None
+    tooltip_border_radius: int
+    tooltip_fancy_mode: bool
+    inner_radius: float
+    half_pie: bool
+    reverse_direction: bool
+    x_labels: list[str] | None
+    x_labels_major: list[str] | None
+    x_labels_major_every: int | None
+    x_labels_major_count: int | None
+    show_x_labels: bool
+    show_minor_x_labels: bool
+    y_labels: list[float] | None
+    y_labels_major: list[str] | None
+    y_labels_major_every: int | None
+    y_labels_major_count: int | None
+    show_minor_y_labels: bool
+    show_y_labels: bool
+    x_label_rotation: int
+    y_label_rotation: int
+    missing_value_fill_truncation: str
+    x_value_formatter: Callable[..., str]
+    value_formatter: Callable[..., str]
+    logarithmic: bool
+    interpolate: str | None
+    interpolation_precision: int
+    interpolation_parameters: dict[str, object]
+    box_mode: str
+    order_min: int | None
+    min_scale: int
+    max_scale: int
+    range: tuple[float, float] | list[float] | None
+    secondary_range: tuple[float, float] | list[float] | None
+    xrange: tuple[float, float] | list[float] | None
+    include_x_axis: bool
+    zero: int | float
+    no_data_text: str
+    print_values: bool
+    dynamic_print_values: bool
+    print_values_position: str
+    print_zeroes: bool
+    print_labels: bool
+    truncate_legend: int | None
+    truncate_label: int | None
+    js: list[str]
+    disable_xml_declaration: bool
+    force_uri_protocol: str | None
+    explicit_size: bool
+    pretty_print: bool
+    strict: bool
+    no_prefix: bool
+    inverse_y_axis: bool
 
 class SerieConfig(CommonConfig):
-    title: Incomplete
-    secondary: Incomplete
+    title: str | None
+    secondary: bool
