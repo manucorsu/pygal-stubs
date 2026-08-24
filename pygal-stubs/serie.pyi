1dbc30ea60dbcca55c49f6697ab588c7f6e37ac4
manucorsu <manuelcorsunsky@gmail.com>
2026-08-23 23:32:24 -0300

    serie

--- pygal-stubs/serie.pyi
+++ pygal-stubs/serie.pyi
@@ -1,11 +1,11 @@
-from _typeshed import Incomplete
+from pygal.config import SerieConfig
 from pygal.util import cached_property as cached_property
 
 class Serie:
-    index: Incomplete
-    values: Incomplete
-    config: Incomplete
-    metadata: Incomplete
-    def __init__(self, index, values, config, metadata=None) -> None: ...
+    index: int
+    values: list[object]
+    config: SerieConfig
+    metadata: dict[str, object] | None
+    def __init__(self, index: int, values: list[object], config: SerieConfig, metadata: dict[str, object] | None = None) -> None: ...
     @cached_property
-    def safe_values(self): ...
+    def safe_values(self) -> list[object]: ...
