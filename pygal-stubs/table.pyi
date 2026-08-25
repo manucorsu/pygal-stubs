c459d69a869526e1b69d6b368abe7d0160626dd3
manucorsu <142500898+manucorsu@users.noreply.github.com>
2026-08-25 09:41:04 -0300

    table

--- pygal-stubs/table.pyi
+++ pygal-stubs/table.pyi
@@ -1,12 +1,14 @@
-from _typeshed import Incomplete
+from typing import override
+from pygal import Graph
 from pygal.util import template as template
 
 class HTML:
-    def __getattribute__(self, attr): ...
+    @override
+    def __getattribute__(self, attr: str) -> object: ...
 
 class Table:
-    chart: Incomplete
-    def __init__(self, chart) -> None: ...
+    chart: Graph
+    def __init__(self, chart: Graph) -> None: ...
     def render(
         self, total: bool = False, transpose: bool = False, style: bool = False
-    ): ...
+    ) -> str: ...
