a1098332d81822e77c558ef65a313b470acdeb6c
manucorsu <142500898+manucorsu@users.noreply.github.com>
2026-08-27 07:53:20 -0300

    graph/bar

--- pygal-stubs/graph/bar.pyi
+++ pygal-stubs/graph/bar.pyi
@@ -1,4 +1,5 @@
 from pygal.graph.graph import Graph as Graph
+from pygal.serie import Serie
 from pygal.util import (
     alter as alter,
     decorate as decorate,
@@ -7,4 +8,4 @@ from pygal.util import (
 )
 
 class Bar(Graph):
-    def bar(self, serie, rescale: bool = False) -> None: ...
+    def bar(self, serie: Serie, rescale: bool = False) -> None: ...
