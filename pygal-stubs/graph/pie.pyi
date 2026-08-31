fd9c35aaa125100affd6b48851fdcb6b021e4f41
manucorsu <manuelcorsunsky@gmail.com>
2026-08-31 00:11:24 -0300

    graph/pie, graph/radar, graph/solidgauge

--- pygal-stubs/graph/pie.pyi
+++ pygal-stubs/graph/pie.pyi
@@ -1,8 +1,9 @@
 from pygal.adapters import none_to_zero as none_to_zero
 from pygal.adapters import positive as positive
 from pygal.graph.graph import Graph as Graph
+from pygal.serie import Serie
 from pygal.util import alter as alter
 from pygal.util import decorate as decorate
 
 class Pie(Graph):
-    def slice(self, serie, start_angle, total): ...
+    def slice(self, serie: Serie, start_angle: float, total: float) -> float: ...
--- pygal-stubs/graph/solidgauge.pyi
+++ pygal-stubs/graph/solidgauge.pyi
@@ -1,6 +1,13 @@
 from pygal.graph.graph import Graph as Graph
+from pygal.serie import Serie
 from pygal.util import alter as alter
 from pygal.util import decorate as decorate
 
 class SolidGauge(Graph):
-    def gaugify(self, serie, squares, sq_dimensions, current_square) -> None: ...
+    def gaugify(
+        self,
+        serie: Serie,
+        squares: object,
+        sq_dimensions: tuple[float, float],
+        current_square: tuple[int, int],
+    ) -> None: ...  # squares is never accessed so it can be anything
