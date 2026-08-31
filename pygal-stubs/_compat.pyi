a2201ff808018bb965bc6e938626a1901e322b5e
manucorsu <manuelcorsunsky@gmail.com>
2026-08-31 01:25:49 -0300

    graph/time and associated fixes

--- pygal-stubs/_compat.pyi
+++ pygal-stubs/_compat.pyi
@@ -1,6 +1,7 @@
 from collections.abc import Iterable
 from typing import TypeVar, Protocol
 from typing_extensions import TypeIs
+from datetime import datetime
 
 _T_co = TypeVar("_T_co", covariant=True, default=object)
 
@@ -8,3 +9,4 @@ class _ListLikeIterable(Iterable[_T_co], Protocol):
     pass
 
 def is_list_like(value: object) -> TypeIs[_ListLikeIterable[object]]: ...
+def timestamp(x: datetime) -> float: ...
