a5d2aa1d7f07c57cedbfa3566b7b1c3e4b7f5e79
manucorsu <manuelcorsunsky@gmail.com>
2026-09-02 23:52:23 -0300

    vt&pk: Pyramid

--- pygal-stubs/graph/pyramid.pyi
+++ pygal-stubs/graph/pyramid.pyi
@@ -1,6 +1,7 @@
 from collections.abc import Sequence
 from typing import Generic, TypeVar
 
+from pygal import Config
 from pygal.adapters import positive as positive
 from pygal.graph.horizontal import HorizontalGraph as HorizontalGraph
 from pygal.graph.stackedbar import StackedBar as StackedBar
@@ -11,4 +12,14 @@ _ValueT = TypeVar(
 )
 
 class VerticalPyramid(StackedBar[_ValueT], Generic[_ValueT]): ...
-class Pyramid(HorizontalGraph[Sequence[float | None]], VerticalPyramid): ...
+
+class Pyramid(HorizontalGraph[Sequence[float | None]], VerticalPyramid):
+    def __init__(
+        self,
+        config: Config | type[Config] | None = None,
+        *,
+        title: str | None = None,
+        human_readable: bool | None = None,
+        legend_at_bottom: bool | None = None,
+        **kwargs: object,
+    ) -> None: ...
