dfc4462f030c165174d64d4c627de0dbd22e69fb
manucorsu <manuelcorsunsky@gmail.com>
2026-09-02 23:50:30 -0300

    vt&pk: Gauge

--- pygal-stubs/graph/gauge.pyi
+++ pygal-stubs/graph/gauge.pyi
@@ -1,3 +1,4 @@
+from pygal import Config
 from pygal.graph.graph import Graph as Graph
 from pygal.serie import Serie
 from pygal.util import (
@@ -19,6 +20,15 @@ from pygal.view import (
     PolarThetaView as PolarThetaView,
 )
 
-class Gauge(Graph):
+class Gauge(Graph[float]):
+    def __init__(
+        self,
+        config: Config | type[Config] | None = None,
+        *,
+        title: str | None = None,
+        human_readable: bool | None = None,
+        reverse_direction: bool | None = None,
+        **kwargs: object,
+    ) -> None: ...
     needle_width: float
     def needle(self, serie: Serie) -> None: ...
